824c2cef7652251f6a6d98d9958047f4d2d10643
GeonHee-Lee <ghlee00125@naver.com>
2025-01-07 14:26:30 +0900

    feat: 하트비스 스케줄러 생성

--- src/main/java/mcnc/talkwave/config/WebSocketConfig.java
+++ src/main/java/mcnc/talkwave/config/WebSocketConfig.java
@@ -1,8 +1,10 @@
 package mcnc.talkwave.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,6 +13,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1); // 스레드 풀 크기
+        scheduler.setThreadNamePrefix("WebSocket-Heartbeat-");
+        return scheduler;
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
